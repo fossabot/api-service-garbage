4a3065b52e16c3e32defc13acff3c6eb5024350c
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-11-07 09:19:07 +0100

    UF-6247: Fix dependency issue and compiler warnings.

--- src/test/java/se/sundsvall/garbage/service/GarbageServiceTest.java
+++ src/test/java/se/sundsvall/garbage/service/GarbageServiceTest.java
@@ -15,6 +15,7 @@ import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -22,8 +23,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
 import se.sundsvall.garbage.api.model.FacilityCategory;
 import se.sundsvall.garbage.api.model.GarbageScheduleResponse;
@@ -35,60 +34,59 @@ import se.sundsvall.garbage.service.mapper.Mapper;
 
 @ExtendWith(MockitoExtension.class)
 class GarbageServiceTest {
-    
-    private final Page<GarbageScheduleEntity> garbageSchedulePage = new PageImpl<>(Collections.singletonList(buildGarbageScheduleEntity()));
-    @Mock
-    private Specification<GarbageScheduleEntity> mockSpecification;
-    @Mock
-    private GarbageScheduleRepository repository;
-    @Mock(answer = Answers.CALLS_REAL_METHODS)
-    private Mapper mapper;
-    @Mock
-    private FileHandler fileHandler;
-    @Mock
-    private GarbageScheduleSpecification garbageScheduleSpecification;
-    @InjectMocks
-    private GarbageService service;
-    
-    @Test
-    void getGarbageSchedule() {
-        var request = buildGarbageScheduleRequest();
-        
-        when(garbageScheduleSpecification.createGarbageScheduleSpecification(any())).thenReturn(mockSpecification);
-        when(repository.findAll(any(Specification.class), any(Pageable.class))).thenReturn(garbageSchedulePage);
-        
-        var result = service.getGarbageSchedules(request);
-        
-        assertThat(result).isNotNull();
-        assertThat(result.get(0).getGarbageScheduledDay()).isEqualTo(GarbageScheduleResponse.WeekDay.FRIDAY);
-        assertThat(result.get(0).getGarbageScheduledWeek()).isEqualTo(GarbageScheduleResponse.Week.ODD);
-        assertThat(result.get(0).getAdditionalInformation()).isEqualTo(request.getAdditionalInformation());
-        assertThat(result.get(0).getAddress().getPostalCode()).isEqualTo(request.getPostalCode());
-        assertThat(result.get(0).getAddress().getStreet()).isEqualTo(request.getStreet());
-        assertThat(result.get(0).getAddress().getHouseNumber()).isEqualTo(request.getHouseNumber());
-        assertThat(result.get(0).getAddress().getCity()).isEqualTo(request.getCity());
-        assertThat(result.get(0).getFacilityCategory()).isEqualTo(FacilityCategory.VILLA);
-        
-        verifyNoInteractions(fileHandler);
-        verify(repository, times(1)).findAll(any(Specification.class), any(Pageable.class));
-        verify(mapper, times(1)).entityToResponse(any());
-        verifyNoMoreInteractions(mapper);
-        verifyNoMoreInteractions(repository);
-    }
-    
-    
-    @Test
-    void updateGarbageSchedules() {
-        
-        service.updateGarbageSchedules();
-        
-        verify(fileHandler, times(1)).downloadFile();
-        verify(fileHandler, times(1)).parseFile();
-        verify(repository, times(1)).deleteAllInBatch();
-        verify(repository, times(1)).saveAll(any());
-        verifyNoMoreInteractions(fileHandler);
-        verifyNoMoreInteractions(repository);
-        verifyNoInteractions(mapper);
-        
-    }
+
+	private final Page<GarbageScheduleEntity> garbageSchedulePage = new PageImpl<>(Collections.singletonList(buildGarbageScheduleEntity()));
+	@Mock
+	private Specification<GarbageScheduleEntity> mockSpecification;
+	@Mock
+	private GarbageScheduleRepository repository;
+	@Mock(answer = Answers.CALLS_REAL_METHODS)
+	private Mapper mapper;
+	@Mock
+	private FileHandler fileHandler;
+	@Mock
+	private GarbageScheduleSpecification garbageScheduleSpecification;
+	@InjectMocks
+	private GarbageService service;
+
+	@Test
+	void getGarbageSchedule() {
+		final var request = buildGarbageScheduleRequest();
+
+		when(garbageScheduleSpecification.createGarbageScheduleSpecification(any())).thenReturn(mockSpecification);
+		when(repository.findAll(ArgumentMatchers.<Specification<GarbageScheduleEntity>>any(), any(Pageable.class))).thenReturn(garbageSchedulePage);
+
+		final var result = service.getGarbageSchedules(request);
+
+		assertThat(result).isNotNull();
+		assertThat(result.get(0).getGarbageScheduledDay()).isEqualTo(GarbageScheduleResponse.WeekDay.FRIDAY);
+		assertThat(result.get(0).getGarbageScheduledWeek()).isEqualTo(GarbageScheduleResponse.Week.ODD);
+		assertThat(result.get(0).getAdditionalInformation()).isEqualTo(request.getAdditionalInformation());
+		assertThat(result.get(0).getAddress().getPostalCode()).isEqualTo(request.getPostalCode());
+		assertThat(result.get(0).getAddress().getStreet()).isEqualTo(request.getStreet());
+		assertThat(result.get(0).getAddress().getHouseNumber()).isEqualTo(request.getHouseNumber());
+		assertThat(result.get(0).getAddress().getCity()).isEqualTo(request.getCity());
+		assertThat(result.get(0).getFacilityCategory()).isEqualTo(FacilityCategory.VILLA);
+
+		verifyNoInteractions(fileHandler);
+		verify(repository, times(1)).findAll(ArgumentMatchers.<Specification<GarbageScheduleEntity>>any(), any(Pageable.class));
+		verify(mapper, times(1)).entityToResponse(any());
+		verifyNoMoreInteractions(mapper);
+		verifyNoMoreInteractions(repository);
+	}
+
+	@Test
+	void updateGarbageSchedules() {
+
+		service.updateGarbageSchedules();
+
+		verify(fileHandler, times(1)).downloadFile();
+		verify(fileHandler, times(1)).parseFile();
+		verify(repository, times(1)).deleteAllInBatch();
+		verify(repository, times(1)).saveAll(any());
+		verifyNoMoreInteractions(fileHandler);
+		verifyNoMoreInteractions(repository);
+		verifyNoInteractions(mapper);
+
+	}
 }
